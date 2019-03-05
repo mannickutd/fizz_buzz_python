f449a241776d4433876fdc98f5cb009a31bcaece
Nicholas Staples <nicholas.staples@solution-tribute.com>
2019-03-05 20:50:50 +0000

    Extend for printing pink_flamingo.

--- fizz_buzz_python/fizz_buzz_pink_flamingo.py
+++ fizz_buzz_python/fizz_buzz_pink_flamingo.py
@@ -1,3 +1,20 @@
+import math
+
+
+def is_perfect_square(x):
+    '''
+    https://www.geeksforgeeks.org/check-number-fibonacci-number/
+    ''' 
+    s = int(math.sqrt(x)) 
+    return s*s == x
+
+def is_fibonacci(n):
+    '''
+    https://www.geeksforgeeks.org/check-number-fibonacci-number/
+    '''
+    # n is Fibinacci if one of 5*n*n + 4 or 5*n*n - 4 or both 
+    # is a perferct square 
+    return is_perfect_square(5*n*n + 4) or is_perfect_square(5*n*n - 4)
 
 
 def is_fizz_buzz_number(num:int) -> str:
@@ -5,17 +22,25 @@ def is_fizz_buzz_number(num:int) -> str:
         Return a str representation of the number.
         for multiples of three return “Fizz” instead of the
         number and for multiples of five return “Buzz”.
-        For numbers which are multiples of both three and five return “FizzBuzz”
+        For numbers which are multiples of both three and five return
+        “FizzBuzz”. Return “Flamingo” when a number is a member of
+        the Fibonacci sequence, and “Pink Flamingo” when it is a
+        multiple of 3 and 5 and a member of the Fibonacci sequence.
     '''
     if num <= 0:
         return str(num)
     div_by_three = num % 3
     div_by_five = num % 5
+    fib_num = is_fibonacci(num)
     if div_by_three == 0 and div_by_five == 0:
+        if fib_num:
+            return 'Pink Flamingo'
         return 'FizzBuzz'
-    if div_by_three == 0:
+    elif div_by_three == 0:
         return 'Fizz'
     elif div_by_five == 0:
         return 'Buzz'
+    elif fib_num:
+        return 'Flamingo'
     else:
         return str(num)
--- tests/fizz_buzz_python_/test_fizz_buzz_pink_flamingo.py
+++ tests/fizz_buzz_python_/test_fizz_buzz_pink_flamingo.py
@@ -1,18 +1,51 @@
 import pytest
-from fizz_buzz_python.fizz_buzz_pink_flamingo import is_fizz_buzz_number
+from fizz_buzz_python.fizz_buzz_pink_flamingo import (
+	is_fizz_buzz_number,
+	is_fibonacci,
+	is_perfect_square,
+)
+
+
+@pytest.mark.parametrize("num,is_perfect_square_", [
+	(2, False),
+	(3, False),
+])
+def test_is_perfect_square_sanity(num, is_perfect_square_):
+	assert is_perfect_square(num) == is_perfect_square_
+
+
+@pytest.mark.parametrize("num,is_fib_", [
+    (0, True),
+    (1, True),
+    (2, True),
+    (3, True),
+    (4, False),
+    (5, True),
+    (7, False),
+    (8, True),
+    (9, False),
+    (10, False),
+    (13, True),
+    (15, False),
+    (6765, True),
+])
+def test_is_fibonacci(num, is_fib_):
+	assert is_fibonacci(num) == is_fib_
 
 
 @pytest.mark.parametrize("num,expected_str", [
     (0, '0'),
-    (2, '2'),
+    (2, 'Flamingo'),
     (3, 'Fizz'),
     (4, '4'),
     (5, 'Buzz'),
     (-1, '-1'),
-    (8, '8'),
+    (7, '7'),
+    (8, 'Flamingo'),
     (9, 'Fizz'),
     (10, 'Buzz'),
     (15, 'FizzBuzz'),
+    (6765, 'Pink Flamingo'),
 ])
 def test_is_fizz_buzz_number(num, expected_str):
 	assert is_fizz_buzz_number(num) == expected_str
